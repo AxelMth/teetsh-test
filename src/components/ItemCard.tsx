2782401139fda44b4de51921b4f0ce3698c0e631
Axel Mathieu-Le Gall <aujoulator@hotmail.fr>
2025-10-15 12:29:56 +0200

    Enhance ItemCard component by adding a Badge for item status display. Update layout with improved padding and adjust background color based on status for better visual clarity.

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Text } from '@chakra-ui/react';
+import { Badge, Card, Text } from '@chakra-ui/react';
 import { Item } from '../intefaces/programmation';
 
 type ItemCardProps = {
@@ -26,16 +26,23 @@ export const ItemCard = ({ item }: ItemCardProps) => {
       size="sm"
       shadow="sm"
       borderRadius="md"
-      bg={statusColors.bg}
       mb="2"
     >
-      <Card.Body p="2">
+      <Card.Body p="2" pr="5">
         <Text
           fontSize="xs"
-          color={statusColors.color}
           fontWeight="medium"
           dangerouslySetInnerHTML={{ __html: item.value }}
         />
+        <Badge 
+          top="0"
+          right="0"
+          position="absolute"
+          borderBottomLeftRadius="md"
+          backgroundColor={statusColors.bg}
+          height="4px"
+          colorScheme={statusColors.color}>{item.status}
+        </Badge>
       </Card.Body>
     </Card.Root>
   );
